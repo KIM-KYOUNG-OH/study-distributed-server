0c4740237e4cd443ef210cd326b67ac8933fb74d
김경오 <kdkg1234@sjsofttech.co.kr>
2023-09-13 08:54:16 +0900

    .

--- httpserver/src/main/java/org/example/WebServer.java
+++ httpserver/src/main/java/org/example/WebServer.java
@@ -1,12 +1,18 @@
 package org.example;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final String TASK_ENDPOINT = "/task";
@@ -19,6 +25,18 @@ public class WebServer {
         this.port = port;
     }
 
+    public static void main(String[] args) {
+        int serverPort = 8080;
+        if (args.length == 1) {
+            serverPort = Integer.parseInt(args[0]);
+        }
+
+        WebServer webServer = new WebServer(serverPort);
+        webServer.startServer();
+
+        System.out.println("Server is Listening on Port " + serverPort);
+    }
+
     public void startServer() {
         try {
             this.server = HttpServer.create(new InetSocketAddress(port), 0);
@@ -29,6 +47,59 @@ public class WebServer {
 
         HttpContext statusContext = server.createContext(STATUS_ENDPOINT);
         HttpContext taskContext = server.createContext(TASK_ENDPOINT);
+
+        statusContext.setHandler(this::handleStatusCheckRequest);
+        taskContext.setHandler(this::handleTaskRequest);
+
+        server.setExecutor(Executors.newFixedThreadPool(8));
+        server.start();
+    }
+
+    private void handleTaskRequest(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            exchange.close();
+            return;
+        }
+
+        Headers headers = exchange.getRequestHeaders();
+        if (headers.containsKey("X-Test") && headers.get("X-Test").get(0).equalsIgnoreCase("true")) {
+            String dummyResponse = "123\n";
+            sendResponse(dummyResponse.getBytes(), exchange);
+            return;
+        }
+
+        boolean isDebugMode = false;
+        if (headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) {
+            isDebugMode = true;
+        }
+
+        long startTime = System.nanoTime();
+
+        InputStream requestBody = exchange.getRequestBody();
+        byte[] requestBytes = new byte[requestBody.available()];
+        byte[] responseBytes = calculateResponse(requestBytes);
+
+        long finishTime = System.nanoTime();
+
+        if (isDebugMode) {
+            String debugMessage = String.format("Operation took %d ms\n", finishTime - startTime);
+            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMessage));
+        }
+
+        sendResponse(responseBytes, exchange);
+    }
+
+    private byte[] calculateResponse(byte[] requestBytes) {
+        String bodyString = new String(requestBytes);
+        String[] stringNumber = bodyString.split(",");
+
+        BigInteger result = BigInteger.ONE;
+        for (String number : stringNumber) {
+            BigInteger bigInteger = new BigInteger(number);
+            result = result.multiply(bigInteger);
+        }
+
+        return String.format("Result of the multiplication is %s\n", result).getBytes();
     }
 
     public void handleStatusCheckRequest(HttpExchange exchange) throws IOException {

89432bf57c49846658769a4013779b2334c25612
김경오 <kdkg1234@sjsofttech.co.kr>
2023-09-12 09:28:16 +0900

    .

---
+++ httpserver/src/main/java/org/example/WebServer.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class WebServer {
+    private static final String TASK_ENDPOINT = "/task";
+    private static final String STATUS_ENDPOINT = "/status";
+
+    private final int port;
+    private HttpServer server;
+
+    public WebServer(int port) {
+        this.port = port;
+    }
+
+    public void startServer() {
+        try {
+            this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        HttpContext statusContext = server.createContext(STATUS_ENDPOINT);
+        HttpContext taskContext = server.createContext(TASK_ENDPOINT);
+    }
+
+    public void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
+            exchange.close();
+            return;
+        }
+
+        String responseMessage = "Server is alive";
+        sendResponse(responseMessage.getBytes(), exchange);
+    }
+
+    private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(responseBytes);
+        outputStream.flush();
+        outputStream.close();
+    }
+
+}

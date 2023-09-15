01c1e673ee0827580102c50459fd5692e95820d5
김경오 <kdkg1234@sjsofttech.co.kr>
2023-09-15 09:30:19 +0900

    tf-idf 구현

---
+++ tf-idf/src/main/java/SequentialSearch.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SequentialSearch {
+    public static final String BOOKS_DIRECTORY = "./resources/books";
+    public static final String SEARCH_QUERY1 = "The best detective that catches many criminals using his deductive methods";
+    public static final String SEARCH_QUERY2 = "The girl the falls through a rabbit hole into a fantasy wonderland";
+    public static final String SEARCH_QUERY3 = "A war between Russia and France in the cold winter";
+
+    public static void main(String[] args) {
+        File documentDirectory = new File(BOOKS_DIRECTORY);
+
+        Arrays.asList()
+    }
+}

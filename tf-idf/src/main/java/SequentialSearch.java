aa9301bdf5e7607f50092dc37b8278daa440efb9
김경오 <kdkg1234@sjsofttech.co.kr>
2023-09-19 08:33:45 +0900

    tf-idf 구현

--- tf-idf/src/main/java/SequentialSearch.java
+++ tf-idf/src/main/java/SequentialSearch.java
@@ -1,4 +1,15 @@
+import model.DocumentData;
+import search.TFIDF;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SequentialSearch {
     public static final String BOOKS_DIRECTORY = "./resources/books";
@@ -6,9 +17,40 @@ public class SequentialSearch {
     public static final String SEARCH_QUERY2 = "The girl the falls through a rabbit hole into a fantasy wonderland";
     public static final String SEARCH_QUERY3 = "A war between Russia and France in the cold winter";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File documentDirectory = new File(BOOKS_DIRECTORY);
 
-        Arrays.asList()
+        List<String> documents = Arrays.asList(documentDirectory.list())
+                .stream()
+                .map(documentName -> BOOKS_DIRECTORY + "/" + documentName)
+                .collect(Collectors.toList());
+
+        List<String> terms = TFIDF.getWordsFromLine(SEARCH_QUERY1);
+
+        findMostRelevantDocuments(documents, terms);
+    }
+    
+    private static void findMostRelevantDocuments(List<String> documents, List<String> terms) throws FileNotFoundException {
+        Map<String, DocumentData> documentsResults = new HashMap<>();
+
+        for (String document : documents) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(document));
+            List<String> lines = bufferedReader.lines().collect(Collectors.toList());
+            List<String> words = TFIDF.getWordsFromLine(lines);
+            DocumentData documentData = TFIDF.createDocumentData(words, terms);
+            documentsResults.put(document, documentData);
+        }
+
+        Map<Double, List<String>> documentsByScore = TFIDF.getDocumentsSortedByScore(terms, documentsResults);
+        printResult(documentsByScore);
+    }
+
+    private static void printResult(Map<Double, List<String>> documentsByScore) {
+        for (Map.Entry<Double, List<String>> docScorePair : documentsByScore.entrySet()) {
+            Double score = docScorePair.getKey();
+            for (String document : docScorePair.getValue()) {
+                System.out.println(String.format("Book : %s - score : %f", document.split("/")[3], score));
+            }
+        }
     }
 }

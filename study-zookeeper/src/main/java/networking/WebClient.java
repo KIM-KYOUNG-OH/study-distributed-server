93b56b7a9c64dfcfb140debf17b625e033163f58
김경오 <kdkg1234@sjsofttech.co.kr>
2023-09-26 09:07:44 +0900

    TF-IDF 분산 시스템 구현

--- study-zookeeper/src/main/java/networking/WebClient.java
+++ study-zookeeper/src/main/java/networking/WebClient.java
@@ -1,4 +1,30 @@
 package networking;
 
+import com.sun.deploy.net.HttpRequest;
+import model.Result;
+import model.SerializationUtils;
+import sun.net.www.http.HttpClient;
+
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+
 public class WebClient {
+    private HttpClient httpClient;
+
+    public WebClient() {
+        this.httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+    }
+
+    public CompletableFuture<Result> sendTask(String url, byte[] requestPayload) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofByteArray(requestPayload))
+                .uri(URI.create(url))
+                .build();
+
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofByteArray())
+                .thenApply(HttpResponse::body)
+                .thenApply(responseBody -> (Result) SerializationUtils.deserialize(responseBody));
+    }
 }
--- study-zookeeper/src/main/java/search/SearchCoordinator.java
+++ study-zookeeper/src/main/java/search/SearchCoordinator.java
@@ -1,12 +1,18 @@
 package search;
 
 import cluster.management.ServiceRegistry;
+import model.Result;
+import model.SerializationUtils;
+import model.Task;
 import networking.OnRequestCallback;
 import networking.WebClient;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class SearchCoordinator implements OnRequestCallback {
@@ -32,6 +38,63 @@ public class SearchCoordinator implements OnRequestCallback {
         return ENDPOINT;
     }
 
+    private List<Result> sendTasksToWorkers(List<String> workers, List<Task> tasks) {
+        CompletableFuture<Result>[] futures = new CompletableFuture[workers.size()];
+        for (int i = 0; i < workers.size(); i++) {
+            String worker = workers.get(i);
+            Task task = tasks.get(i);
+            byte[] payload = SerializationUtils.serialize(task);
+
+            futures[i] = client.sendTask(worker, payload);
+        }
+
+        List<Result> results = new ArrayList<>();
+        for (CompletableFuture<Result> future : futures) {
+            try {
+                Result result = future.get();
+                results.add(result);
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println(String.format("Received %d/%d results", results.size(), tasks.size()));
+        return results;
+    }
+
+    public List<Task> createTasks(int numberOfWorkers, List<String> searchTerms) {
+        List<List<String>> workersDocuments = splitDocumentList(numberOfWorkers, documents);
+
+        List<Task> tasks = new ArrayList<>();
+
+        for (List<String> documentsForWorker : workersDocuments) {
+            Task task = new Task(searchTerms, documentsForWorker);
+            tasks.add(task);
+        }
+
+        return tasks;
+    }
+
+    private static List<List<String>> splitDocumentList(int numberOfWorkers, List<String> documents) {
+        int numberOfDocumentsPerWorker = (documents.size() + numberOfWorkers - 1) / numberOfWorkers;
+
+        List<List<String>> workersDocuments = new ArrayList<>();
+
+        for (int i = 0; i < numberOfWorkers; i++) {
+            int firstDocumentIndex = i * numberOfDocumentsPerWorker;
+            int lastDocumentIndexExclusive = Math.min(firstDocumentIndex + numberOfDocumentsPerWorker, documents.size());
+
+            if (firstDocumentIndex >= lastDocumentIndexExclusive) {
+                break;
+            }
+
+            List<String> currentWorkerDocuments = new ArrayList<>(documents.subList(firstDocumentIndex, lastDocumentIndexExclusive));
+
+            workersDocuments.add(currentWorkerDocuments);
+        }
+        return workersDocuments;
+    }
+
     private List<String> readDocumentsList() {
         File documentDirectory = new File(BOOKS_DIRECTORY);
         return Arrays.asList(documentDirectory.list())
@@ -39,4 +102,5 @@ public class SearchCoordinator implements OnRequestCallback {
                 .map(documentName -> BOOKS_DIRECTORY + "/" + documentName)
                 .collect(Collectors.toList());
     }
+
 }
